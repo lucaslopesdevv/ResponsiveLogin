d68eed4f7fcb935a2dce92dc8eaf12e78ba31acd
Lucas Fabricio Lopes <94392411+lucaslopesdevv@users.noreply.github.com>
2022-12-22 15:55:35 -0300

    new update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { Header } from "../../components/Header/Header";
 
-export default function Home() {
-  return <div>Você está no painel do chamado</div>;
+import styles from "./Home.module.css";
+import logoCrediceripa from "../../assets/logo-Crediceripa.svg";
+
+export function Home() {
+  return (
+    <div className={styles.homeMain}>
+      <Header />
+      <div className={styles.homePainel}>
+        <header>Portal do chamado</header>
+        <div className={styles.homePainelItems}></div>
+      </div>
+      <footer></footer>
+    </div>
+  );
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,12 +5,15 @@ import {
 } from "react-router-dom";
 
 import { Login } from "../pages/Login/index";
+import { Home } from "../pages/Home";
+import "../global.css";
 
 export function Routes() {
   return (
     <Router>
       <Rotas>
         <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
       </Rotas>
     </Router>
   );
